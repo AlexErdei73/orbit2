d42db21b053fca306fbe055c9fb633bb61a03899
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-02-03 23:40:21 +0000

    Add second planet to the simulation without interaction

--- src/Orbit2.java
+++ src/Orbit2.java
@@ -5,12 +5,12 @@ import java.awt.event.ActionListener;
 class Orbit2 extends Plot implements Runnable{
     static double rMax = 2; // max range of plot
     double tolerance;
-    double x1;
-    double y1;
-    double v1x;
-    double v1y;
-    double a1x;
-    double a1y;
+    double x1, x2;
+    double y1, y2;
+    double v1x, v2x;
+    double v1y, v2y;
+    double a1x, a2x;
+    double a1y, a2y;
     double totalEnergy;
     Image savedOffscreenImage;
     boolean running;
@@ -22,11 +22,17 @@ class Orbit2 extends Plot implements Runnable{
         this.y1 = 0;
         this.v1x = 0;
         this.v1y = 0.75*2*Math.PI;
+        this.x2 = 1.5;
+        this.y2 = 0;
+        this.v2x = 0;
+        this.v2y = 1*2*Math.PI/Math.sqrt(1.5);
         this.totalEnergy = this.totalE();
         System.out.print("Energy: ");
         System.out.println(this.totalEnergy);
-        a1x = this.ax();
-        a1y = this.ay();
+        a1x = this.ax(1);
+        a1y = this.ay(1);
+        a2x = this.ax(2);
+        a2y = this.ay(2);
         this.setPointSize(1);
         this.savedOffscreenImage = createImage(this.plotHeight + 1, this.plotWidth + 1);
         this.savedOffscreenImage.getGraphics().drawImage(this.offScreenImage, 0, 0, this);
@@ -59,47 +65,83 @@ class Orbit2 extends Plot implements Runnable{
         }
     }
 
-    private double ax() {
+    private double ax(int n) {
         double G = 4 * Math.PI * Math.PI;
-        return -G * this.x1 * Math.pow((this.x1 * this.x1 + this.y1 * this.y1), -1.5);
+        double x, y;
+        if (n == 1) {
+            x = this.x1;
+            y = this.y1;
+        } else {
+            x = this.x2;
+            y = this.y2;
+        }
+        return -G * x * Math.pow((x * x + y * y), -1.5);
     }
 
-    private double ay() {
+    private double ay(int n) {
         double G = 4 * Math.PI * Math.PI;
-        return -G * this.y1 * Math.pow((this.x1 * this.x1 + this.y1 * this.y1), -1.5);
+        double x, y;
+        if (n == 1) {
+            x = this.x1;
+            y = this.y1;
+        } else {
+            x = this.x2;
+            y = this.y2;
+        }
+        return -G * y * Math.pow((x * x + y * y), -1.5);
     }
 
     private double totalE() {
         double G = 4 * Math.PI * Math.PI;
-        double kinE = 0.5 * (this.v1x * this.v1x + this.v1y * this.v1y);
-        double potE = - G * Math.pow((this.x1 * this.x1 + this.y1 * this.y1), -0.5);
-        return kinE + potE;
+        double kinE1 = 0.5 * (this.v1x * this.v1x + this.v1y * this.v1y);
+        double kinE2 = 0.5 * (this.v2x * this.v2x + this.v2y * this.v2y);
+        double potE1 = - G * Math.pow((this.x1 * this.x1 + this.y1 * this.y1), -0.5);
+        double potE2 = - G * Math.pow((this.x2 * this.x2 + this.y2 * this.y2), -0.5);
+        return kinE1 + potE1 + kinE2 + potE2;
     }
 
     synchronized void doStep() {
+        // clean images of planets
+        offScreenGraphics.drawImage(savedOffscreenImage, 0, 0, this);
         // execute one step of simulation
-        double dt = this.tolerance * (this.x1*this.x1 + this.y1*this.y1);
+        double dt1 = this.tolerance * (this.x1*this.x1 + this.y1*this.y1);
+        double dt2 = this.tolerance * (this.x2*this.x2 + this.y2*this.y2);
+        double dt = Math.min(dt1, dt2);
         this.x1 += this.v1x * dt + 0.5 * this.a1x * dt * dt;
         this.y1 += this.v1y * dt + 0.5 * this.a1y * dt * dt;
         this.v1x += 0.5 * this.a1x * dt;
         this.v1y += 0.5 * this.a1y * dt;
-        this.a1x = this.ax();
-        this.a1y = this.ay();
+        this.a1x = this.ax(1);
+        this.a1y = this.ay(1);
         this.v1x += 0.5 * this.a1x * dt;
         this.v1y += 0.5 * this.a1y * dt;
+        this.setColor(Color.red);
         this.addPoint(this.x1, this.y1);
+        this.x2 += this.v2x * dt + 0.5 * this.a2x * dt * dt;
+        this.y2 += this.v2y * dt + 0.5 * this.a2y * dt * dt;
+        this.v2x += 0.5 * this.a2x * dt;
+        this.v2y += 0.5 * this.a2y * dt;
+        this.a2x = this.ax(2);
+        this.a2y = this.ay(2);
+        this.v2x += 0.5 * this.a2x * dt;
+        this.v2y += 0.5 * this.a2y * dt;
+        this.setColor(Color.green);
+        this.addPoint(this.x2, this.y2);
+        // save image without the planets
+        savedOffscreenImage.getGraphics().drawImage(offScreenImage, 0, 0, this);
+        this.showPlanets();
     }
 
-    public void addPoint(double x, double y) {
-        offScreenGraphics.drawImage(savedOffscreenImage, 0, 0, this);
-        super.addPoint(x, y);
-        savedOffscreenImage.getGraphics().drawImage(offScreenImage, 0, 0, this);
+    private void showPlanets() {
         // draw planets at current position
         int size = 7;
-        int pixelx = (int) Math.round(plotWidth * (this.x1-xMin) / xRange);	// convert x1 to a screen coordinate
-        int pixely = (int) Math.round(plotHeight * (yMax-this.y1) / yRange);
+        int pixelx = (int) Math.round(plotWidth * (x1-xMin) / xRange);	// convert x1 to a screen coordinate
+        int pixely = (int) Math.round(plotHeight * (yMax-y1) / yRange);
         int offset = (int) (size/2.0);
         offScreenGraphics.setColor(Color.blue);
+        offScreenGraphics.drawOval(pixelx - offset, pixely - offset, size - 1, size - 1);
+        pixelx = (int) Math.round(plotWidth * (x2-xMin) / xRange);	// convert x2 to a screen coordinate
+        pixely = (int) Math.round(plotHeight * (yMax-y2) / yRange);
         offScreenGraphics.drawOval(pixelx - offset, pixely - offset, size - 1, size - 1);
         offScreenGraphics.setColor(this.pointColor);
         repaint();
